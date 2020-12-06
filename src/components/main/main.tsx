2e903742409065564b1b0cba712b34f5450de225
Yongcheon Kim <honey.yong.1988@gmail.com>
2020-12-06 21:44:57 +0900

    init react, typescirpt

---
+++ src/components/main/main.tsx
@@ -0,0 +1,8 @@
+import React, {ReactNode} from 'react';
+import {Component} from 'react';
+
+export default class Main extends Component {
+    public render(): ReactNode {
+        return <div>main</div>;
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    mode: "development",
+  
+    // 엔트리 포인트
+    entry: "./src/app.tsx",
+  
+    // 빌드 결과물을 dist/main.js에 위치
+    output: {
+      filename: "app.js",
+      path: __dirname + "/docs/js",
+    },
+  
+    // 디버깅을 위해 빌드 결과물에 소스맵 추가
+    devtool: "source-map",
+  
+    resolve: {
+      // 파일 확장자 처리
+      extensions: [".ts", ".tsx", ".js"],
+    },
+  
+    module: {
+      rules: [
+        // .ts나 .tsx 확장자를 ts-loader가 트랜스파일
+        { test: /\.tsx?$/, loader: "ts-loader" },
+      ],
+    },
+  }
